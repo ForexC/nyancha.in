c6a7a97260d5251725d8abdf8d65e1725fd60812
Hendrik 'Bloody' Schumann <bloody@fluffl.es>
2015-04-06 01:06:24 +0200

    Subtle nyan.js performance improvement

--- Abe/htdocs/nyan.js
+++ Abe/htdocs/nyan.js
@@ -1,29 +1,40 @@
+window.requestAnimFrame = (function(){
+	return window.requestAnimationFrame ||
+			window.webkitRequestAnimationFrame ||
+			window.mozRequestAnimationFrame ||
+			function(callback ){
+			window.setTimeout(callback, 1000 / 60);
+			};
+})();
+
 var nyans = 0;
 
 var createNyan = function () {
-  var left = -100;
-  var top = Math.random() * 100;
-  var curr = nyans;
+	var left = -100;
+	var top = Math.random() * 100;
+	var curr = nyans;
 
-  var nyan = document.createElement('div');
-  nyan.setAttribute('class', 'nyan');
-  nyan.setAttribute('id', 'nyan' + curr);
-  nyan.setAttribute('style', 'left: ' + left + 'px; top: ' + top + '%');
-  nyan.innerHTML = '<img src="/static/nyan.gif" height="50" width="100" alt="">';
-  document.getElementById('nyans').appendChild(nyan);
+	var nyan = document.createElement('div');
+	nyan.setAttribute('class', 'nyan');
+	nyan.setAttribute('id', 'nyan' + curr);
+	nyan.setAttribute('style', 'left: ' + left + 'px; top: ' + top + '%');
+	nyan.innerHTML = '<img src="/static/nyan.gif" height="50" width="100" alt="">';
+	document.getElementById('nyans').appendChild(nyan);
 
-  var interval = setInterval(function () {
-    left += 2;
-    document.getElementById('nyan' + curr).style.left = left + 'px';
+	var update = (function () {
+		left += 2;
+		nyan.style.left = left + 'px';
 
-    if (left === window.innerWidth) {
-      clearInterval(interval);
-      nyan.remove();
-      setTimeout(createNyan, 5000 + Math.random() * 5000);
-    }
-  }, 12);
+		if (left === window.innerWidth) {
+			nyan.remove();
+			setTimeout(createNyan, 5000 + Math.random() * 5000);
+		} else {
+			requestAnimFrame(update);
+		}
+	});
+	requestAnimFrame(update);
 
-  nyans++;
+	nyans++;
 };
 
 setTimeout(createNyan, Math.random() * 10000);
